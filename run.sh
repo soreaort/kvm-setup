9b707241d6a1a62e325048848b579ee451bac5e1
Saul Orea <soreaort@gmail.com>
2018-10-03 20:52:29 -0500

    Fix typo
    Signed-off-by: Saul Orea <soreaort@gmail.com>

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 USERNAME=$1
 HOST=$2
 PORT=$3
-ssh -Tq ${USERNAME}@${HOST} -p ${PORT} -o StrictHostKeyChecking no 'sudo sh -s' < setup.sh
+ssh -Tq ${USERNAME}@${HOST} -p ${PORT} -o StrictHostKeyChecking=no 'sudo sh -s' < setup.sh
